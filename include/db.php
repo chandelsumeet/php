da784d6a6841eeab2214f24a54f3bc17e8c18534
chandelsumeet <sumeetchandel321@gmail.com>
2020-04-06 17:36:06 +0530

    first commit

---
+++ include/db.php
@@ -0,0 +1,23 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="cms";
+
+$conn=new mysqli($servername,$username,$password,$database);
+
+// if($conn->connect_error)
+// {
+// 	die("connection failed");
+// }
+// else
+// {
+// 	echo "connected successfully";
+// }
+
+
+
+
+
+
+?>
